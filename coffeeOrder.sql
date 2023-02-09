7ffb1a167260739a595dd08445f8f4f919af44ef
Adan Olaad <adan.olaad@ppluk.com>
2023-02-09 11:05:24 +0000

    Created and modified the SQL file.

---
+++ coffeeOrder.sql
@@ -0,0 +1,15 @@
+-- Table contains details of coffee orders.
+
+CREATE Table orders (
+    order_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    order_name VARCHAR (30),
+    order_type VARCHAR (30),
+    cream BOOLEAN, 
+    number_orders INTEGER,
+    order_cost  FLOAT
+
+);
+
+INSERT INTO orders (order_name, order_type, cream, number_orders, order_cost) VALUES ('Jammie Tart', 'Chai Latte', true, 2, 8.0);
+INSERT INTO orders (order_name, order_type, cream, number_orders, order_cost) VALUES ('Dani Rojas', 'Espresso', false, 1, 4.50);
+INSERT INTO orders (order_name, order_type, cream, number_orders, order_cost) VALUES ('Roy Kent', 'Hot Choc', true, 2, 3.50);
