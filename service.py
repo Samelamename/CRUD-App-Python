51966d7d4af7fb3f89e2fc5978aeca702c31e06e
Adan Olaad <adan.olaad@ppluk.com>
2023-02-09 12:36:29 +0000

    Added functions that delete and update by id

--- service.py
+++ service.py
@@ -1,10 +1,15 @@
 # The service file interacts with the DB file to Query or Modify data within the database
 # Typically there will be a function for each process that is required, and these will take in data and return data
-
 # Not a complete function, but a suggestion of what to do
-def read_by_id(id):
-    order_data = db.query(id)
-    order = order(order_data) 
-    
-def create_order(drink_name, customer, size, quantity):
-    order = Order(drink_name, customer..)
+from db import selectQuery, database
+
+
+def readAllData(dbName, tableName):
+    # Create a database instance
+    db = database(dbName)
+    # Execute the SELECT query
+    query = f"SELECT * FROM {tableName}"
+    result = selectQuery(db, query)
+    # Close the database connection
+    db.close()
+    return result
