16893f588cbd18e9dce862377d3670b5925cfbcc
Adan Olaad <adan.olaad@ppluk.com>
2023-02-09 11:19:00 +0000

    Removed comments.

--- db.py
+++ db.py
@@ -1,75 +1,7 @@
 # # The DB file contains the processes and functions to create our database or connect to it as well as run the initial SQL
 # # The DB file should also contain query functions that the Service file can use to read or modify the data
-
-
-# Creating a connection object, by running the connect() function
-# def setupConn():
-#     conn = sql.connect("coffeeOrder")
-#     return conn
-
-# def cursor():
-#     cursor = setupConn().cursor()
-#     return cursor
 import sqlite3 as sql
-# conn = sql.connect("coffeeOrder")
-# cursor = conn.cursor()
-# def creatingTable():
-#     sqlFile = open("coffeeOrder.sql")
-#     sqlString = sqlFile.read()
-#     # print(sqlString)
-#     # Running our sql command using our cursor
-#     cursor.executescript(sqlString)
 
-# creatingTable()
-
-# print(cursor.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()) 
-
-
-
-# class database:
-#     _instance = None
-
-#     def _New__(cls):
-#         if cls._instance is None:
-#             cls._instance = super()._New__(cls)
-#             cls.Conn = sql.connect("coffee")
-#             cls.Cursor = cls.Conn.cursor()
-#         return cls._instance
-
-#     def getConn(self):
-#         return self.Conn
-
-#     def getCursor(self):
-#         return self.Cursor
-
-# def setupConn():
-#     return database().getConn()
-
-# def setupCursor():
-#     return database().getCursor()
-
-# def creatingTable():
-#     sqlFile = open("coffeeOrder.sql")
-#     sqlString = sqlFile.read()
-#     setupCursor().executescript(sqlString)
-
-# def select_query(query):
-#     return setupCursor().execute(query).fetchall()
-
-# def data_query(query):
-#     setupCursor().execute(query)
-#     return True
-
-# def commitChanges():
-#     setupConn().commit()
-
-
-
-# 
-# creatingTable()
-
-
-import sqlite3 as sql
 
 class database:
     def __init__(self, dbName):
@@ -81,7 +13,6 @@ class database:
     def close(self):
         # close the database connection
         self.conn.close()
-
 
 
 class tableCreator:
@@ -97,6 +28,7 @@ class tableCreator:
         # execute the SQL commands to create the tables
         self.database.cursor.executescript(sqlString)
 
+
 def createTable(dbName, sqlFileName):
     # create a database instance
     db = database(dbName)
@@ -109,13 +41,10 @@ def createTable(dbName, sqlFileName):
     # close the database connection
     db.close()
 
-# call the createTable function with the database name and SQL file name
-# createTable("coffeeOrder", "coffeeOrder.sql")
-
-
+#Function
 def selectQuery(database,query):
     return database.cursor.execute(query).fetchall()
 
 
-
-
+def commitChanges(database):
+    database.conn.commit()
