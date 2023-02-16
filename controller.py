fdf07dc747eab69499708cc763202876c9aba5ec
Adan Olaad <adan.olaad@ppluk.com>
2023-02-16 12:22:59 +0000

    Reformated the intial print statement of application

--- controller.py
+++ controller.py
@@ -4,29 +4,30 @@
 # It will run commands from the service file, which in turn uses the DB file to 
 # query and create data and will return the data back to the user
 from service import *
-# print(
-#     """
-#     Welcome to the Olaad Cafe, what would you like to do? 
-
-#     1. Create an order
-#     2. Read an order
-#     3. Read all Orders
-#     4. Update an order
-#     5. Delete an order
-#     6. Delete all orders
-#     """
-# )
 db = initializeDb("coffeeOrder")
+print(
+    f"""
+    Welcome to the Olaad Cafe,
+    Here is all the orders in the database currently: 
+    {readAllData(db, "orders")}
+    What would you like to do?
+    1. Create an order
+    2. Read an order
+    3. Read all Orders
+    4. Update an order
+    5. Delete an order
+    """
+)
+
 # Call the readAllData function with the database name and table name
 # createTable(db, "coffeeOrder.sql")
 
-
-print(readAllData(db, "orders"))
+deleteOrderId(db)
 
 # updateOrderNameId(db, 2, "Ted Lasso")
 
 # createOrder(db)
 
-# print(readAllData(db, "orders"))
+print(readAllData(db, "orders"))
 commitChanges(db)
 db.close()
