3d9eb9776cea8147d08f830fa0058769218fa28f
Adan Olaad <adan.olaad@ppluk.com>
2023-02-16 11:25:37 +0000

    Renamed order name to customer name

--- coffeeOrder.sql
+++ coffeeOrder.sql
@@ -2,7 +2,7 @@
 
 CREATE Table orders (
     order_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    order_name VARCHAR (30),
+    customer_name VARCHAR (30),
     order_type VARCHAR (30),
     cream BOOLEAN, 
     number_orders INTEGER,
@@ -10,6 +10,6 @@ CREATE Table orders (
 
 );
 
-INSERT INTO orders (order_name, order_type, cream, number_orders, order_cost) VALUES ('Jammie Tart', 'Chai Latte', true, 2, 8.0);
-INSERT INTO orders (order_name, order_type, cream, number_orders, order_cost) VALUES ('Dani Rojas', 'Espresso', false, 1, 4.50);
-INSERT INTO orders (order_name, order_type, cream, number_orders, order_cost) VALUES ('Roy Kent', 'Hot Choc', true, 2, 3.50);
+INSERT INTO orders (customer_name, order_type, cream, number_orders, order_cost) VALUES ('Jammie Tart', 'Chai Latte', true, 2, 8.0);
+INSERT INTO orders (customer_name, order_type, cream, number_orders, order_cost) VALUES ('Dani Rojas', 'Espresso', false, 1, 4.50);
+INSERT INTO orders (customer_name, order_type, cream, number_orders, order_cost) VALUES ('Roy Kent', 'Hot Choc', true, 2, 3.50);
